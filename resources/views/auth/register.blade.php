a22c49b5dcda2160551d05693aa9dfb17d956d21
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-16 23:56:47 +0800

    fixed add admind and super admin

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Welcome Admin') }}</div>
+                    <div class="card-header"><a href="/admin"> {{ __('Go back') }}</a></div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
